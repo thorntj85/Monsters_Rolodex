30121c1e639448e55e7e70c7fefc125ce50aa09d
thorntj85 <54555269+thorntj85@users.noreply.github.com>
2019-10-23 23:51:41 +0100

    Updates

---
+++ precache-manifest.921a27ee26ef5ef400dbfba5825cf395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2fcf78c977c6dd8d0454ac06f1387b",
+    "url": "/Monsters_Rolodex/index.html"
+  },
+  {
+    "revision": "fb95ac6edc40e9182f2a",
+    "url": "/Monsters_Rolodex/static/css/main.f3069e11.chunk.css"
+  },
+  {
+    "revision": "193f5db9d2ba56bbd4fe",
+    "url": "/Monsters_Rolodex/static/js/2.c10ccd50.chunk.js"
+  },
+  {
+    "revision": "fb95ac6edc40e9182f2a",
+    "url": "/Monsters_Rolodex/static/js/main.15e8f75c.chunk.js"
+  },
+  {
+    "revision": "2403d3aaf41b977b6ecb",
+    "url": "/Monsters_Rolodex/static/js/runtime-main.64c4e4cf.js"
+  }
+]);
